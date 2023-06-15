93c846a901b51e28758d90d3462caa50c424ead1
rafiq <mds23568@gmail.com>
2023-06-15 20:07:56 +0600

    tailwind project

---
+++ js/tailwind.config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'primary': "#203239" ,
+          'highlighter': "#F6762E",
+          'bannerbg':"#EEEDDE",
+          'peracolor':"#2032398C",
+          'inputcolor':"#fff",
+          'pluscolor':"#FF7527",
+          'btnopacity_color':"rgba(255, 117, 39, 0.2);",
+          'btnopacity_color_two':"rgba(255, 117, 39, 0.8);",
+        },
+        fontFamily: {
+           'popins':"'Poppins', sans-serif;"
+          },
+        maxWidth: {
+            'container': '1166px',
+        },
+        lineHeight: {
+            'headinghight': '84px',
+          }
+      }
+    }
+  }
